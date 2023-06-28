eee09177a4a787fcd9171f589440447a44eee976
brGuirra <br.guirra@gmail.com>
2023-06-27 22:29:46 -0300

    feat: add styles to history page

---
+++ src/pages/history/index.tsx
@@ -0,0 +1,63 @@
+import { HistoryContainer, HistoryListWrapper, Status } from './styles';
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryListWrapper>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor='yellow'>Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor='red'>Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryListWrapper>
+    </HistoryContainer>
+  );
+}
